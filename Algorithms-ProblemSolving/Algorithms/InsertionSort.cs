f4338b09a1f565b07dc441fc00f766e6f8be47e2
Ahmed <sanada927@gmail.com>
2026-03-10 22:15:42 +0200

    chore: add insertion sort implementation

---
+++ Algorithms-ProblemSolving/Algorithms/InsertionSort.cs
@@ -0,0 +1,19 @@
+﻿namespace Algorithms_ProblemSolving.Algorithms
+{
+    public class InsertionSort
+    {
+        public static void Run(int[] numbers, Func<int, int, bool> compare)
+        {
+            for(int j = 1; j < numbers.Length; j++)
+            {
+                int key = numbers[j], i = j - 1;
+                while(i >= 0 && compare(key, numbers[i]))
+                {
+                    numbers[i + 1] = numbers[i];
+                    i--;
+                }
+                numbers[i + 1] = key;
+            }
+        }
+    }
+}
--- Algorithms-ProblemSolving/Program.cs
+++ Algorithms-ProblemSolving/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Algorithms_ProblemSolving
+﻿using Algorithms_ProblemSolving.Algorithms;
+
+namespace Algorithms_ProblemSolving
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = { 5, 2, 3, 6, 7, 1 };
+            
+            InsertionSort.Run(numbers, (a, b) => a < b);
+            Console.WriteLine(string.Join(',', numbers));
+
+            InsertionSort.Run(numbers, (a, b) => a > b);
+            Console.WriteLine(string.Join(',', numbers));
         }
     }
 }
